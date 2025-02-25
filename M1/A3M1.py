0f5db5adfc65422f5c95b92963a6884c15e1620c
Nicholas <nchoang1@uci.edu>
2025-02-24 18:02:46 -0800

    updating branch a3m1

--- M1/A3M1.py
+++ M1/A3M1.py
@@ -2,17 +2,22 @@ import parse as p
 import tokenizer as t
 import index
 import nltk
+import time
 
+# "C:\\Users\\fight\\OneDrive\\Desktop\\CS121\\Assignment3\\CS121_SearchEngine\\DEV"
 def main() -> None:
     """
     Run the program
     """
-    count = 0
-    files = p.traverse_directory("C:\\Users\\fight\\OneDrive\\Desktop\\CS121\\Assignment3\\CS121_SearchEngine\\DEV\\aiclub_ics_uci_edu")
-    for file in files:
-        frequency_dict = p.parse(file)
-        print(frequency_dict)
-        count += 1
+    start_time = time.time()
+    files = p.traverse_directory("C:\\Users\\Justi\\Downloads\\CS121\\Assignment3M1\\cs_121_A3\\DEV")
+    index_maker = index.Indexer()
+    index_maker.run(files)
+    end_time = time.time()  # Record the end time
+    elapsed_time = end_time - start_time  # Compute duration
+    print(f"Execution time: {elapsed_time:.6f} seconds")
+
+
 
 if __name__ == "__main__":
     main()
